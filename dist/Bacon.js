636d0a4afb3cc54a0bade13544f50d9338c2aa2a
Juha Paananen <juha.paananen@gmail.com>
2014-10-23 18:47:00 +0300

    release 0.7.25

--- dist/Bacon.js
+++ dist/Bacon.js
@@ -11,7 +11,7 @@
     }
   };
 
-  Bacon.version = '0.7.24';
+  Bacon.version = '0.7.25';
 
   Exception = (typeof global !== "undefined" && global !== null ? global : this).Error;
 
@@ -3048,7 +3048,7 @@
       return Bacon;
     });
     this.Bacon = Bacon;
-  } else if (typeof module !== "undefined" && module !== null) {
+  } else if ((typeof module !== "undefined" && module !== null) && (module.exports != null)) {
     module.exports = Bacon;
     Bacon.Bacon = Bacon;
   } else {
