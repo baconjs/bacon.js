baconjs/bacon.js

commit d49fd51cffc7d77262b1706a8c04e53d2c81a093

re-impl toProperty as lazy

src/Bacon.coffee
@@ -758,9 +758,38 @@ class EventStream extends Observable
     left = this
     withDescription(left, "merge", right, Bacon.mergeAll(this, right))
 
-  toProperty: (initValue) ->
-    initValue = None if arguments.length == 0
-    withDescription(this, "toProperty", initValue, @scan(initValue, latter))
+  toProperty: (initValue_) ->
+    initValue = if arguments.length == 0 then None else toOption(-> initValue_)
+    disp = @dispatcher
+    new Property(describe(this, "toProperty", initValue),
+      (sink) ->
+        initSent = false
+        unsub = nop
+        reply = Bacon.more
+        sendInit = ->
+          unless initSent
+            initValue.forEach (value) ->
+              initSent = true
+              reply = sink (new Initial(value))
+              if reply == Bacon.noMore
+                unsub()
+                unsub = nop
+        unsub = disp.subscribe (event) ->
+          if event.hasValue()
+            if (initSent and event.isInitial())
+              Bacon.more
+            else
+              sendInit() unless event.isInitial()
+              initSent = true
+              initValue = new Some(event.value)
+              sink event
+          else
+            if event.isEnd()
+              reply = sendInit()
+            sink event unless reply == Bacon.noMore
+        sendInit()
+        unsub
+    )
 
   toEventStream: -> this
 

commit 251d9d47c888a55c99542dfdc04fbc28c7063a85

fix bug introduced in d49fd51cffc7d77262b1706a8c04e53d2c81a093

src/Bacon.coffee
@@ -308,7 +308,7 @@ class Event
 class Next extends Event
   constructor: (valueF, eager) ->
     super()
-    if !eager && isFunction(valueF) || valueF instanceof Initial
+    if !eager && isFunction(valueF) || valueF instanceof Next
       @valueF = valueF
       @valueInternal = undefined
     else
@@ -317,7 +317,7 @@ class Next extends Event
   isNext: -> true
   hasValue: -> true
   value: ->
-    if @valueF instanceof Initial
+    if @valueF instanceof Next
       @valueInternal = @valueF.value()
       @valueF = undefined
     else if @valueF
@@ -781,7 +781,7 @@ class EventStream extends Observable
             else
               sendInit() unless event.isInitial()
               initSent = true
-              initValue = new Some(event.value)
+              initValue = new Some(event)
               sink event
           else
             if event.isEnd()

spec/BaconSpec.coffee
@@ -1649,6 +1649,16 @@ describe "Property.debounce", ->
     expectPropertyEvents(
       -> Bacon.constant(1).debounce(1)
       [1])
+  it "works with Bacon.combine (bug fix)", ->
+    values = []
+    p1 = Bacon.once(true).toProperty()
+    p2 = Bacon.once(true).toProperty()
+
+    visibleP = Bacon.combineAsArray([p1, p2]).startWith(false)
+
+    visibleP.debounce(500).onValue (val)  ->
+      values.push(val)
+
   it "toString", ->
     expect(Bacon.constant(0).debounce(1).toString()).to.equal("Bacon.constant(0).debounce(1)")
 describe "Property.throttle", ->