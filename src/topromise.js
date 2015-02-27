56ae0ab6b1cf4f4f19a9cd6da7d281501c01c436
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-02-27 11:12:47 +0200

    Make toPromise take a promise constructor parameter

--- src/topromise.js
+++ src/topromise.js
@@ -1,8 +1,17 @@
 // build-dependencies: observable
 // build-dependencies: last
 
-Observable.prototype.firstToPromise = function () {
-  return new Promise((resolve, reject) =>
+Observable.prototype.firstToPromise = function (PromiseCtr) {
+  // Can't do in the global scope, as shim can be applied after Bacon is loaded.
+  if (typeof PromiseCtr !== "function") {
+    if (typeof Promise === "function") {
+      PromiseCtr = Promise;
+    } else {
+      throw new Exception("There isn't default Promise, use shim or parameter");
+    }
+  }
+
+  return new PromiseCtr((resolve, reject) =>
     this.subscribe((event) => {
       if (event.hasValue()) { resolve(event.value()); }
       if (event.isError()) { reject(event.error); }
@@ -12,6 +21,6 @@ Observable.prototype.firstToPromise = function () {
     }));
 };
 
-Observable.prototype.toPromise = function () {
-  return this.last().firstToPromise();
+Observable.prototype.toPromise = function (PromiseCtr) {
+  return this.last().firstToPromise(PromiseCtr);
 };
