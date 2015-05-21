b4c04c62f0c8eaa6d4118cce0ee76f0576b3e067
Oskar Ojala <oskar.ojala@reaktor.fi>
2015-05-22 00:38:50 +0300

    release 0.7.59

--- dist/Bacon.noAssert.js
+++ dist/Bacon.noAssert.js
@@ -21,7 +21,7 @@
             return 'Bacon';
         }
     };
-    Bacon.version = '<version>';
+    Bacon.version = '0.7.59';
     Exception = (typeof global !== 'undefined' && global !== null ? global : this).Error;
     nop = function () {
     };
