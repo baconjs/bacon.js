87b5d6fe92db55ca7a086e54f7294a868e21766c
Zoli Issam <jawbfl@gmail.com>
2015-08-02 12:40:41 +0100

    fix for webpack loading - issue #621

--- dist/Bacon.js
+++ dist/Bacon.js
@@ -11,7 +11,7 @@
     }
   };
 
-  Bacon.version = '0.7.70';
+  Bacon.version = '<version>';
 
   Exception = (typeof global !== "undefined" && global !== null ? global : this).Error;
 
@@ -3422,11 +3422,13 @@ if ((typeof define !== "undefined" && define !== null) && (define.amd != null)) 
       return Bacon;
     });
     this.Bacon = Bacon;
-  } else if ((typeof module !== "undefined" && module !== null) && (module.exports != null)) {
+  }
+
+  if ((typeof module !== "undefined" && module !== null) && (module.exports != null)) {
     module.exports = Bacon;
     Bacon.Bacon = Bacon;
-  } else {
-    this.Bacon = Bacon;
   }
+
+  this.Bacon = Bacon;
 
 }).call(this);
