bb1f98950c8fcca7293dcff452102bc98c9c4f75
Juha Paananen <juha.paananen@gmail.com>
2015-09-09 14:51:34 +0300

    release 0.7.72

--- dist/Bacon.noAssert.js
+++ dist/Bacon.noAssert.js
@@ -21,7 +21,7 @@
             return 'Bacon';
         }
     };
-    Bacon.version = '<version>';
+    Bacon.version = '0.7.72';
     Exception = (typeof global !== 'undefined' && global !== null ? global : this).Error;
     nop = function () {
     };
