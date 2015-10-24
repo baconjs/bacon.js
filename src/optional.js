952f5fcaf3ddbccb0ffa140521dc98461e6593c1
Ville Lautanala <lautis@gmail.com>
2015-10-24 15:14:45 +0300

    ES6ify optional

---
+++ src/optional.js
@@ -0,0 +1,46 @@
+function Some(value) {
+  this.value = value;
+}
+
+_.extend(Some.prototype, {
+  _isSome: true,
+  getOrElse() { return this.value; },
+  get() { return this.value; },
+  filter(f) {
+    if (f(this.value) ) {
+      return new Some(this.value);
+    } else {
+      return None;
+    }
+  },
+  map(f) {
+    return new Some(f(this.value));
+  },
+  forEach(f) {
+    return f(this.value);
+  },
+  isDefined: true,
+  toArray() { return [this.value]; },
+  inspect() { return "Some(" + this.value + ")"; },
+  toString() { return this.inspect(); }
+});
+
+var None = {
+  _isNone: true,
+  getOrElse(value) { return value; },
+  filter() { return None; },
+  map() { return None; },
+  forEach() {},
+  isDefined: false,
+  toArray() { return []; },
+  inspect() { return "None"; },
+  toString() { return this.inspect(); }
+};
+
+var toOption = function(v) {
+  if (((typeof v !== "undefined" && v !== null) ? v._isSome : undefined) || ((typeof v !== "undefined" && v !== null) ? v._isNone : undefined)) {
+    return v;
+  } else {
+    return new Some(v);
+  }
+};
