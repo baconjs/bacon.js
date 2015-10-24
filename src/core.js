02eacc57c3b9c900e16deb0462d445a5acd50d7d
Ville Lautanala <lautis@gmail.com>
2015-10-24 13:43:54 +0300

    ES6ify Property dispatcher

--- src/core.js
+++ src/core.js
@@ -4,7 +4,6 @@
 // build-dependencies: reply
 // build-dependencies: observable
 // build-dependencies: eventstream
-// build-dependencies: propertydispatcher
 // build-dependencies: property
 // build-dependencies: frombinder
 // build-dependencies: describe
--- src/property.js
+++ src/property.js
@@ -1,9 +1,81 @@
 // build-dependencies: observable
 // build-dependencies: describe
 // build-dependencies: functionconstruction
-// build-dependencies: propertydispatcher
+// build-dependencies: updatebarrier
+// build-dependencies: dispatcher
 // build-dependencies: optional
 // build-dependencies: helpers
+// build-dependencies: _
+
+function PropertyDispatcher(property, subscribe, handleEvent) {
+  Dispatcher.call(this, subscribe, handleEvent);
+  this.property = property;
+  this.subscribe = _.bind(this.subscribe, this);
+  this.current = None;
+  this.currentValueRootId = undefined;
+  this.propertyEnded = false;
+}
+
+_.extendClass(PropertyDispatcher, Dispatcher);
+_.extend(PropertyDispatcher.prototype, {
+  push(event) {
+    if (event.isEnd()) {
+      this.propertyEnded = true;
+    }
+    if (event.hasValue()) {
+      this.current = new Some(event);
+      this.currentValueRootId = UpdateBarrier.currentEventId();
+    }
+    return Dispatcher.prototype.push.call(this, event);
+  },
+
+  maybeSubSource(sink, reply) {
+    if (reply === Bacon.noMore) {
+      return nop;
+    } else if (this.propertyEnded) {
+      sink(endEvent());
+      return nop;
+    } else {
+      return Dispatcher.prototype.subscribe.call(this, sink);
+    }
+  },
+
+  subscribe(sink) {
+    var initSent = false;
+    // init value is "bounced" here because the base Dispatcher class
+    // won't add more than one subscription to the underlying observable.
+    // without bouncing, the init value would be missing from all new subscribers
+    // after the first one
+    var reply = Bacon.more;
+
+    if (this.current.isDefined && (this.hasSubscribers() || this.propertyEnded)) {
+      // should bounce init value
+      var dispatchingId = UpdateBarrier.currentEventId();
+      var valId = this.currentValueRootId;
+      if (!this.propertyEnded && valId && dispatchingId && dispatchingId !== valId) {
+        // when subscribing while already dispatching a value and this property hasn't been updated yet
+        // we cannot bounce before this property is up to date.
+        //console.log "bouncing with possibly stale value", event.value(), "root at", valId, "vs", dispatchingId
+        UpdateBarrier.whenDoneWith(this.property, () => {
+          if (this.currentValueRootId === valId) {
+            return sink(initialEvent(this.current.get().value()));
+          }
+        });
+        // the subscribing thing should be defered
+        return this.maybeSubSource(sink, reply);
+      } else {
+        //console.log "bouncing value immediately"
+        UpdateBarrier.inTransaction(undefined, this, function() {
+          reply = sink(initialEvent(this.current.get().value()));
+          return reply;
+        }, []);
+        return this.maybeSubSource(sink, reply);
+      }
+    } else {
+      return this.maybeSubSource(sink, reply);
+    }
+  }
+});
 
 function Property(desc, subscribe, handler) {
   Observable.call(this, desc);
