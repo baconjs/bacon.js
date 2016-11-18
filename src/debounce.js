17e86740a3b82b3c0f2bbb6e4596e57045113293
Juha Paananen <juha.paananen@gmail.com>
2016-11-18 11:02:15 +0200

    formatting

--- src/debounce.js
+++ src/debounce.js
@@ -3,11 +3,15 @@
 
 Bacon.EventStream.prototype.debounce = function(delay) {
   return withDesc(new Bacon.Desc(this, "debounce", [delay]), this.flatMapLatest(function(value) {
-    return Bacon.later(delay, value);
-  }));
-};
+    return Bacon.later(delay, value)
+  }))
+}
 
-Bacon.Property.prototype.debounce = function(delay) { return this.delayChanges(new Bacon.Desc(this, "debounce", [delay]), function(changes) { return changes.debounce(delay); }); };
+Bacon.Property.prototype.debounce = function(delay) { 
+  return this.delayChanges(new Bacon.Desc(this, "debounce", [delay]), function(changes) { 
+    return changes.debounce(delay)
+  })
+}
 
 Bacon.EventStream.prototype.debounceImmediate = function(delay) {
   return withDesc(new Bacon.Desc(this, "debounceImmediate", [delay]), this.flatMapFirst(function(value) {
