27b272b7772dfff0a5674e83640a456845486957
Juha Paananen <juha.paananen@gmail.com>
2017-12-20 10:14:19 +0200

    add missing dependency later->scheduler

--- src/later.js
+++ src/later.js
@@ -1,4 +1,4 @@
-// build-dependencies: frombinder
+// build-dependencies: frombinder, scheduler
 Bacon.later = function(delay, value) {
   return withDesc(new Bacon.Desc(Bacon, "later", [delay, value]), Bacon.fromBinder(function(sink) {
     var sender = function() { return sink([value, endEvent()]); };
