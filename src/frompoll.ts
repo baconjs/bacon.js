e4e96c5c44778413c4fcf6cb25b6af3056be3ffc
Juha Paananen <juha.paananen@gmail.com>
2018-08-06 19:58:25 +0300

    doc fix

--- src/frompoll.ts
+++ src/frompoll.ts
@@ -3,15 +3,22 @@ import { Desc } from "./describe";
 import GlobalScheduler from "./scheduler";
 
 /**
+ * A polled function used by [fromPoll](../globals.html#frompoll)
+ */
+export interface PollFunction<V> {
+  (): EventLike<V>
+}
+
+/**
  Polls given function with given interval.
  Function should return Events: either [`Bacon.Next`](classes/next.html) or [`Bacon.End`](classes/end.html). Polling occurs only
  when there are subscribers to the stream. Polling ends permanently when
  `f` returns [`Bacon.End`](classes/end.html).
- * @param poll interval in milliseconds
- * @param poll function
+ * @param delay poll interval in milliseconds
+ * @param poll function to be polled
  * @typeparam V Type of stream elements
  */
-export default function fromPoll<V>(delay: number, poll: () => EventLike<V>) {
+export default function fromPoll<V>(delay: number, poll: PollFunction<V>) {
   var desc = new Desc("Bacon", "fromPoll", [delay, poll]);
   return fromBinder((function (handler) {
     var id = GlobalScheduler.scheduler.setInterval(handler, delay);
