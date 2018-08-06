79384ff675e4324a2cb2982aa4d8b031fe7d398d
Juha Paananen <juha.paananen@gmail.com>
2018-08-06 10:59:06 +0300

    release 2.0.8

--- dist/Bacon.js
+++ dist/Bacon.js
@@ -2051,7 +2051,7 @@ function once(value) {
 Bacon.once = once;
 
 var syncEventStream = function (desc, subscribe) {
-  return new EventStream(dest, subscribe, undefined, allowSync);
+  return new EventStream(desc, subscribe, undefined, allowSync);
 };
 
 Observable.prototype.flatMap_ = function (f) {
--- dist/Bacon.noAssert.js
+++ dist/Bacon.noAssert.js
@@ -1884,7 +1884,7 @@
     }
     Bacon.once = once;
     var syncEventStream = function (desc, subscribe) {
-        return new EventStream(dest, subscribe, undefined, allowSync);
+        return new EventStream(desc, subscribe, undefined, allowSync);
     };
     Observable.prototype.flatMap_ = function (f) {
         var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
