73987f00a685e8d7eb8bb877135dbf3bb8067c19
Juha Paananen <juha.paananen@gmail.com>
2024-07-01 20:23:18 +0300

    Improve Bacon.try documentation

--- src/try.ts
+++ src/try.ts
@@ -3,8 +3,20 @@ import { Error } from "./event";
 import { EventStream } from "./observable";
 
 /** 
- * A helper for creating an EventStream of a single value, or a single Error event in case the given
- * function throws an exception.
+ `Bacon.try` is a helper for creating an EventStream of a single value, or a single Error event in case the given
+ function throws an exception.
+
+For example, you can use `Bacon.try` to handle JSON parse errors:
+
+```js
+var jsonStream = Bacon
+  .once('{"this is invalid json"')
+  .flatMap(Bacon.try(JSON.parse))
+
+jsonStream.onError(function(err) {
+  console.error("Failed to parse JSON", err)
+})
+
  */
 export default function tryF<In, Out>(f: (value: In) => Out): (value: In) => EventStream<Out> {
   return function(value) {
